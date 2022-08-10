78aa6bb35db40f43d4ffc64548412e787e6978d5
Losalini <c.rokocakau@gmail.com>
2022-08-11 05:22:01 +1200

    Dynamically create list of tasks in index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import _ from 'lodash';
 import './style.css';
 
+const ul = document.querySelector('ul');
+const tasks = [{
+    description: 'Task 1',
+    completed: false,
+    index: 1
+  },
+  {
+    description: 'Task 2',
+    completed: false,
+    index: 2
+  }, 
+  {
+    description: 'Task 3',
+    completed: false,
+    index: 3
+  }
+];
+
+tasks.forEach((task) => {
+  const li = document.createElement('li');
+
+  li.innerHTML = `
+    <div class="task">
+      <input type="checkbox" id="task-${task.index}" name="task-${task.index}" value=""/>
+      <label for="task-${task.index}">Task ${task.index}</label>
+    </div>
+    <i class="material-icons">more_vert</i>
+  `;
+
+  ul.appendChild(li);
+});
