6ac517b7fffde5ec643a95930a5d55fa9f4f56e3
Losalini <c.rokocakau@gmail.com>
2022-08-11 05:47:01 +1200

    Fix all linter errors within local environment

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-import _ from 'lodash';
 import './style.css';
 
 const ul = document.querySelector('ul');
 const tasks = [{
-    description: 'Task 1',
-    completed: false,
-    index: 1
-  },
-  {
-    description: 'Task 2',
-    completed: false,
-    index: 2
-  }, 
-  {
-    description: 'Task 3',
-    completed: false,
-    index: 3
-  },
-  {
-    description: 'Task 4',
-    completed: false,
-    index: 4
-  }
+  description: 'Task 1',
+  completed: false,
+  index: 1,
+},
+{
+  description: 'Task 2',
+  completed: false,
+  index: 2,
+},
+{
+  description: 'Task 3',
+  completed: false,
+  index: 3,
+},
+{
+  description: 'Task 4',
+  completed: false,
+  index: 4,
+},
 ];
 
 tasks.forEach((task) => {
@@ -33,7 +32,7 @@ tasks.forEach((task) => {
       <input type="text" name="task-${task.index} value="${task.description}" placeholder="${task.description}"/>
     </div>
     <button>
-      <i class="material-icons">more_vert</i>
+      <i class="vertical-line-icon material-icons">more_vert</i>
     </button>
   `;
 
