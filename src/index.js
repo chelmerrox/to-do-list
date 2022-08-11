f7e4d4a20c466af2139cdfd757c88451ee86cc92
Losalini <c.rokocakau@gmail.com>
2022-08-12 02:52:38 +1200

    Add addTask(), displayTask() and removeTask() functions

--- src/index.js
+++ src/index.js
@@ -1,44 +1,89 @@
 import './style.css';
 
+const formSubmission = document.getElementById('form');
+const tasks = [];
+let indexOne = 0;
 const ul = document.querySelector('ul');
-const tasks = [{
-  description: 'Task 1',
-  completed: false,
-  index: 1,
-},
-{
-  description: 'Task 2',
-  completed: false,
-  index: 2,
-},
-{
-  description: 'Task 3',
-  completed: false,
-  index: 3,
-},
-{
-  description: 'Task 4',
-  completed: false,
-  index: 4,
-},
-];
 
-const createList = () => {
-  tasks.forEach((task) => {
-    const li = document.createElement('li');
+const removeTask = (taskObject, index) => {
+  const li = document.getElementById(index);
 
-    li.innerHTML = `
-      <div class="task">
-        <input type="checkbox" id="task-${task.index}" name="task-${task.index}" value=""/>
-        <input type="text" name="task-${task.index} value="${task.description}" placeholder="${task.description}"/>
-      </div>
-      <button>
-        <i class="vertical-line-icon material-icons">more_vert</i>
-      </button>
-    `;
+  const { description, completed, indexOne } = taskObject;
 
-    ul.appendChild(li);
-  });
+  tasks.filter((task) => {
+    task.description !== description && task.completed !== completed && task.index !== indexOne
+   })
+
+   localStorage.setItem('tasksArray', JSON.stringify(tasks));
+   ul.removeChild(li);
 };
 
-createList();
+const displayTask = (taskObject, index) => {
+  const li = document.createElement('li');
+  li.id = index;
+
+  li.innerHTML = `
+    <div class="task">
+      <input type="checkbox" id="task-${taskObject.index}" name="task-${taskObject.index}" value=""/>
+      <input type="text" name="task-${taskObject.index} value="${taskObject.description}" placeholder="${taskObject.description}"/>
+    </div>
+  `;
+
+  const verticalLineBtn = document.createElement('button');
+  verticalLineBtn.classList.add('vertical-line-btn');
+  verticalLineBtn.style.border = 'none';
+  verticalLineBtn.style.backgroundColor = 'white';
+
+  const verticalLineIcon = document.createElement('i');
+  verticalLineIcon.classList.add('material-icons');
+  verticalLineIcon.innerHTML = 'more_vert';
+  verticalLineIcon.style.color = '#b2b2b2';
+
+  verticalLineBtn.appendChild(verticalLineIcon);
+  li.appendChild(verticalLineBtn);
+
+  verticalLineBtn.onclick = () => {
+    const removeIcon = document.createElement('i');
+    removeIcon.classList.add('material-icons');
+    removeIcon.innerHTML = 'delete';
+    removeIcon.style.color = '#b2b2b2';
+
+    verticalLineBtn.removeChild(verticalLineIcon);
+    verticalLineBtn.appendChild(removeIcon);
+
+    verticalLineBtn.onclick = () => {
+      removeTask(taskObject, index);
+    };
+  };
+  
+  ul.appendChild(li);
+};
+
+const addTask = (taskObject) => {
+  indexOne = (tasks.length === 0)? 1 : tasks.length + 1;
+
+  taskObject.index = indexOne;
+
+  tasks.push(taskObject);
+  
+  localStorage.setItem('tasksArray', JSON.stringify(tasks));
+
+  displayTask(taskObject, tasks.length - 1);
+}
+
+formSubmission.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const addTaskInput = document.querySelector('.add-task-input'); 
+
+  if (addTaskInput.value !== ''){
+    const taskObject = {
+      description: addTaskInput.value,
+      completed: false
+    }
+
+    addTask(taskObject);
+  }
+
+  formSubmission.reset();
+});
