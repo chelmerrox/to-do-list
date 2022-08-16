dc538102350c5491373efc1deeeeb05f7e45f12e
Losalini <c.rokocakau@gmail.com>
2022-08-17 00:03:46 +1200

    Fix all linter errors within local environment

--- src/index.js
+++ src/index.js
@@ -3,21 +3,19 @@ import './style.css';
 import Task from './modules/task.js';
 
 const formSubmission = document.getElementById('form');
-//let i = 0;
-export const ul = document.querySelector('ul');
 
 const taskOne = new Task(); // instance for a task
 
 formSubmission.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const addTaskInput = document.querySelector('.add-task-input'); 
+  const addTaskInput = document.querySelector('.add-task-input');
 
-  if (addTaskInput.value !== ''){
+  if (addTaskInput.value !== '') {
     const taskObject = {
-      description: addTaskInput.value,
-      completed: false
-    }
+      name: addTaskInput.value,
+      isDone: false,
+    };
 
     taskOne.addTask(taskObject);
   }
--- src/modules/task.js
+++ src/modules/task.js
@@ -1,29 +1,32 @@
-import { ul } from './../index.js';
-
+const ul = document.querySelector('ul');
 let i = 0;
 
 export default class Task {
-  constructor (tasks = []) {
+  constructor(tasks = []) {
     this.tasks = tasks;
     this.getFromLocal();
   }
 
-  changeElementAndIcon(taskObject, verticalLineBtn, verticalLineIcon){
+  changeElementAndIcon(taskObject, verticalLineBtn, verticalLineIcon, li) {
+    li.classList.add('active');
+
     const taskContainer = document.querySelector(`.task-${taskObject.i}`);
-  
+
     const p = document.querySelector(`.task-to-edit-${taskObject.i}`);
-  
+
     taskContainer.removeChild(p);
-  
+
     const taskToEditInput = document.createElement('input');
     taskToEditInput.setAttribute('type', 'text');
     taskToEditInput.setAttribute('name', `task-to-edit-${taskObject.i}`);
-    taskToEditInput.setAttribute('value',`${taskObject.description}`);
+    taskToEditInput.setAttribute('value', `${taskObject.name}`);
     taskToEditInput.classList.add(`task-to-edit-${taskObject.i}`);
+    taskToEditInput.classList.add('task-to-edit');
+    taskToEditInput.classList.add('active');
     taskToEditInput.setAttribute('autocomplete', 'off');
-  
+
     taskContainer.appendChild(taskToEditInput);
-  
+
     const removeIcon = document.createElement('i');
     removeIcon.classList.add('material-icons');
     removeIcon.classList.add('remove-icon');
@@ -31,145 +34,120 @@ export default class Task {
 
     verticalLineBtn.removeChild(verticalLineIcon);
     verticalLineBtn.appendChild(removeIcon);
-  
+
     taskToEditInput.addEventListener('change', (e) => {
       e.preventDefault();
-  
-      taskObject.description = e.target.value;
-  
+
+      taskObject.name = e.target.value;
+
       localStorage.setItem('tasksArray', JSON.stringify(this.tasks));
     });
-  
+
     taskToEditInput.addEventListener('keypress', (e) => {
-      if (e.key === 'Enter'){
+      if (e.key === 'Enter') {
         taskContainer.removeChild(taskToEditInput);
         this.editTask(taskObject, taskToEditInput.value);
       }
     });
   }
-  
-  displayEditedTask(editedTaskObject, index){
-    const taskContainer2 = document.querySelector(`.task-${editedTaskObject.i}`);
-  
+
+  displayEditedTask = (editedTaskObject, index) => {
+    const taskContainer2 = document.querySelector(`.task-${index + 1}`);
+
     const p2 = document.createElement('p');
-    p2.classList.add(`.task-to-edit-${editedTaskObject.i}`);
-    p2.innerText = `${editedTaskObject.description}`;
-  
+    p2.classList.add(`.task-to-edit-${index + 1}`);
+    p2.innerText = `${editedTaskObject.name}`;
+
     taskContainer2.appendChild(p2);
-  
-    /* const verticalLineBtn2 = document.getElementById(`vertical-line-btn-${editedTaskObject.i}`);
-  
-    const verticalLineIcon2 = document.createElement('i');
-    verticalLineIcon2.classList.add('material-icons');
-    verticalLineIcon2.classList.add('vertical-line-icon');
-    verticalLineIcon2.innerHTML = 'more_vert';
-  
-    const removeIcon2 = document.querySelector('.remove-icon');
-  
-    verticalLineBtn2.removeChild(removeIcon2);
-    verticalLineBtn2.appendChild(verticalLineIcon2);
-  
-    // Click on the vertical line button once to show Remove icon & change <p> to <input>
-    verticalLineBtn2.onclick= () => {
-      this.changeElementAndIcon(editedTaskObject, verticalLineBtn2, verticalLineIcon2);
-  
-      // Clicking on this again removes the task from the list
-      verticalLineBtn.onclick = () => {
-        this.removeTask(editedTaskObject, index);
-      }; 
-    } */
 
     document.location.reload();
   }
-  
-  editTask(taskObject, taskToEditInputValue){
+
+  editTask(taskObject, taskToEditInputValue) {
     this.tasks = JSON.parse(localStorage.getItem('tasksArray'));
-    
-    let taskObjectIndex = taskObject.i;
-  
+
+    const taskObjectIndex = taskObject.i;
+
     this.tasks.forEach((task, k) => {
       if (k === taskObjectIndex - 1) {
-        task.description = taskToEditInputValue;
-  
+        task.name = taskToEditInputValue;
+
         localStorage.setItem('tasksArray', JSON.stringify(this.tasks));
-  
-        this.displayEditedTask(task, this.tasks.length - 1);
+
+        this.displayEditedTask(task, k);
       }
     });
   }
-  
-  removeTask(taskObject, index){
+
+  removeTask(taskObject, index) {
     const list = document.getElementById(index + 1);
-  
-    // needed so that we're able to filter & store the new result in local storage
-    //taskObject.completed = true;
-  
-    const { description, completed, i } = taskObject;
-  
-    this.tasks = this.tasks.filter((task) => task.description !== description && task.completed === completed && task.i !== i );
-  
-    // Change the value of i & store in the i property for the specific task
-    this.tasks.map((task, j) => {
+
+    const { name, i } = taskObject;
+
+    this.tasks = this.tasks.filter((task) => task.name !== name && task.i !== i);
+
+    this.tasks.forEach((task, j) => {
       task.i = j + 1;
     });
-  
+
     localStorage.setItem('tasksArray', JSON.stringify(this.tasks));
     ul.removeChild(list);
-  };
-  
-  displayTask(taskObject, index){
+  }
+
+  displayTask(taskObject, index) {
     const li = document.createElement('li');
     li.id = index + 1;
-  
+
     li.innerHTML = `
       <div class="task task-${taskObject.i}">
         <input type="checkbox" id="task-${taskObject.i}" name="task-${taskObject.i}" value=""/>
-        <p class="task-to-edit-${taskObject.i}">${taskObject.description}</p>
+        <p class="task-to-edit-${taskObject.i}">${taskObject.name}</p>
       </div>
     `;
-  
+
     const verticalLineBtn = document.createElement('button');
     verticalLineBtn.classList.add('vertical-line-btn');
     verticalLineBtn.id = `vertical-line-btn-${taskObject.i}`;
-  
+
     const verticalLineIcon = document.createElement('i');
     verticalLineIcon.classList.add('vertical-line-icon');
     verticalLineIcon.classList.add('material-icons');
     verticalLineIcon.innerHTML = 'more_vert';
-  
+
     verticalLineBtn.appendChild(verticalLineIcon);
     li.appendChild(verticalLineBtn);
-  
+
     // Click on the vertical line button once to show Remove icon & change <p> to <input>
     verticalLineBtn.onclick = () => {
-      this.changeElementAndIcon(taskObject, verticalLineBtn, verticalLineIcon);
-  
+      this.changeElementAndIcon(taskObject, verticalLineBtn, verticalLineIcon, li);
+
       // Clicking on this again removes the task from the list
       verticalLineBtn.onclick = () => {
         this.removeTask(taskObject, index);
-      }; 
+      };
     };
-  
+
     ul.appendChild(li);
-  };
-  
-  addTask(taskObject){
-    i = (this.tasks.length === 0)? 1 : this.tasks.length + 1; //index for taskObject
-    
+  }
+
+  addTask(taskObject) {
+    i = (this.tasks.length === 0) ? 1 : this.tasks.length + 1; // index for taskObject
+
     taskObject.i = i;
-  
+
     this.tasks.push(taskObject);
-    
+
     localStorage.setItem('tasksArray', JSON.stringify(this.tasks));
-  
+
     this.displayTask(taskObject, this.tasks.length - 1);
   }
- 
-  // check local storage before adding a task (upon first visit to page or reload of page in the same browser)
-  getFromLocal(){
+
+  // check local storage before adding a task
+  // (upon first visit to page or reload of page in the same browser)
+  getFromLocal() {
     if (localStorage.getItem('tasksArray')) {
       this.tasks = JSON.parse(localStorage.getItem('tasksArray'));
-  
+
       this.tasks.forEach((taskObject, index) => {
         this.displayTask(taskObject, index);
       });
@@ -177,5 +155,5 @@ export default class Task {
       localStorage.setItem('tasksArray', '');
       this.tasks = [];
     }
-  };
+  }
 }
